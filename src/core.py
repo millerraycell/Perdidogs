0ce5754c0110c177a62430311e1f813fcc0fe3ab
Miller Monteiro <millerraycell@gmail.com>
2020-11-30 18:32:27 -0400

    Adicionando a conexão com twitter

--- src/core.py
+++ src/core.py
@@ -1,52 +1,49 @@
+from pathlib import Path
+
 from pymongo import MongoClient, GEOSPHERE
 
 from uuid import uuid4
 
-from telegram import InlineQueryResultLocation, InlineKeyboardButton, ReplyKeyboardMarkup
+from telegram import InlineQueryResultLocation, ReplyKeyboardMarkup
 from telegram.keyboardbutton import KeyboardButton
-from telegram.ext import InlineQueryHandler, CommandHandler, Filters, MessageHandler, Updater
+from telegram.ext import InlineQueryHandler, CommandHandler, Filters, MessageHandler, Updater 
 from telegram.update import Update
 from telegram.bot import Bot
 from telegram.ext.callbackcontext import CallbackContext
 
+import twitter
 
-from config.settings import TELEGRAM_TOKEN, MONGO_CONNECTION
+from config.settings import TELEGRAM_TOKEN, MONGO_CONNECTION, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET, TWITTER_CONSUMER_TOKEN_KEY, TWITTER_CONSUMER_TOKEN_SECRET
 
+api = twitter.Api(TWITTER_CONSUMER_TOKEN_KEY, TWITTER_CONSUMER_TOKEN_SECRET,TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+
+testes = []
 
 def start(update: Update, context: CallbackContext):
-    response_message = "Deseja enviar a sua localização?"
+    response_message = "Bem-vindo(a) ao Perdidogs\nPedimos que tire uma foto do animal que encontrou"
     bot: Bot = context.bot
-
-    reply_markup = ReplyKeyboardMarkup([[
-        KeyboardButton("Sim", False, True),
-        KeyboardButton("Não"),
-    ]], resize_keyboard=True, one_time_keyboard=True)
 
     bot.send_message(
         chat_id = update.effective_chat.id,
         text = response_message,
-        reply_markup = reply_markup,
     )
 
-def unknown(update: Update, context: CallbackContext):
-    response_message = "Unknown command " + update.message.text
+def tweet(update: Update, context: CallbackContext):
+    response_message = "Mensagem enviada com sucesso"
     bot: Bot = context.bot
 
-    bot.send_message(
-        chat_id = update.effective_chat.id,
-        text = response_message
-    )
+    status = api.PostUpdate('Testeeee', testes[0])
+    print(status)
 
-def my_location (update : Update, context : CallbackContext):
-    bot : Bot = context.bot
-    
-    search = collection.find_one({"user_id": update.message.from_user.id})
-    coord = search["geometry"]["coordinates"]
-    response_message = f"Última localização: Long: {coord[0]} Lat: {coord[1]}"
-    
     bot.sendMessage(chat_id=update.effective_chat.id,
                     text=response_message)
 
+def photo(update: Update, context: CallbackContext):
+    bot: Bot = context.bot
+
+    fileID = update.message.photo[-1].file_id
+    testes.append(bot.get_file(fileID).file_path)
+    
 def location(update: Update, context: CallbackContext):
     bot: Bot = context.bot
 
@@ -57,43 +54,54 @@ def location(update: Update, context: CallbackContext):
             'user_name': update.edited_message.from_user.first_name,
             "geometry": {
                 "type": "Point",
-                "coordinates": [update.edited_message.location.longitude, 
-                                update.edited_message.location.latitude]
+                "coordinates": [update.edited_message.location.latitude, 
+                                update.edited_message.location.longitude]
             }
             
         }
+
+        collection.replace_one(
+            {"user_id": update.message.from_user.id}, 
+            user_location, 
+            upsert= True
+        )
     else:
-        print("Mensagem nova de", update.message.from_user.first_name, update.message.location.longitude, update.message.location.latitude)
+        print("Mensagem nova de", update.message.from_user.first_name, update.message.location.latitude, update.message.location.longitude)
         user_location = {
             'user_id':   update.message.from_user.id,
             'user_name': update.message.from_user.first_name,
             "geometry": {
                 "type": "Point",
-                "coordinates": [update.message.location.longitude, 
-                                update.message.location.latitude]
+                "coordinates": [update.message.location.latitude, 
+                                update.message.location.longitude]
             }
             
         }
+
+        position = "google.com/maps/@{},{},21z".format(update.message.location.latitude, update.message.location.longitude)
+
+        local = api.PostUpdate("Testandooo {}".format(position))
+
+        print(local)
+
+        collection.insert_one(user_location)
     
-    collection.replace_one(
-        {"user_id": update.message.from_user.id}, 
-        user_location, 
-        upsert= True
-    )
+        
 
     bot.send_message(
         chat_id = update.effective_chat.id,
         text = "Localização lida com sucesso"
     )
 
-def inlinequery(update: Update, context :CallbackContext):
-    result = [
-        InlineQueryResultLocation(id= uuid4(),
-            latitude= update.inline_query.location.latitude,
-            longitude= update.inline_query.location.longitude, title="Location")
-    ]
 
-    update.inline_query.answer(result)
+def unknown(update: Update, context: CallbackContext):
+    response_message = "Unknown command " + update.message.text
+    bot: Bot = context.bot
+
+    bot.send_message(
+        chat_id = update.effective_chat.id,
+        text = response_message
+    )
 
 def main():
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
@@ -104,16 +112,16 @@ def main():
         CommandHandler('start', start)
     )
     dispatcher.add_handler(
-        CommandHandler('my_location', my_location)
+        CommandHandler('tweet', tweet)
     )
     dispatcher.add_handler(
         MessageHandler(Filters.location, location)
     )
     dispatcher.add_handler(
-        MessageHandler(Filters.command, unknown)
+        MessageHandler(Filters.photo, photo)
     )
     dispatcher.add_handler(
-        InlineQueryHandler(inlinequery)
+        MessageHandler(Filters.command, unknown)
     )
 
     updater.start_polling()
