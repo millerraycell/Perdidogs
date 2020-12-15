8dcb5703fb0b03617fa796571cce423bf75ea845
Miller Monteiro <millerraycell@gmail.com>
2020-12-15 16:50:02 -0400

    Finalizaçao Frontend

---
+++ web/src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import AnimalsMap from './pages/AnimalsMap';
+import Landing from './pages/Landing';
+
+function Routes(){
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Landing}/>
+                <Route path="/map" component={AnimalsMap}/>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes
